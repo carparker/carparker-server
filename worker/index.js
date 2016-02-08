cd7325f73e37ca6d477908793a65cf68cf16e994
Louis Ledey <lledey@gmail.com>
2016-02-09 00:18:19 +0100

    Remove test worker

--- worker/index.js
+++ worker/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const test = require('./test');
 const opendata = require('./opendata');
 
 require('../modules').mongoose.connect()
   .then(() => {
-    test.start();
     opendata.start();
   });
