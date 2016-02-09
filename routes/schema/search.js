35ee88073320ca01bd6358bc0faab1e4c7e7f199
Louis Ledey <lledey@gmail.com>
2016-02-09 13:37:53 +0100

    Add Joi body validation in /search route

---
+++ routes/schema/search.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Joi = require('joi');
+
+const searchBodySchema = Joi.object().keys({
+  position: Joi.object().keys({
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required()
+  }),
+  address: Joi.string(),
+  radius: Joi.number().positive().required(),
+  price: Joi.object().required().keys({
+    min: Joi.number().min(0).required(),
+    max: Joi.number().min(Joi.ref('min')).required()
+  }),
+  duration: Joi.object().required().keys({
+    min: Joi.number().positive().required()
+  })
+}).xor('position', 'address');
+
+module.exports = searchBodySchema;
--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const httpStatus = require('http-status-codes');
+const Joi = require('joi');
+const logger = require('../modules').logger;
+
+const searchSchema = require('./schema').search;
+
 /**
 {
   position: {
@@ -19,9 +25,14 @@
  */
 
 function search(req, res, next) {
-  // TODO: Joi validation
-  // https://github.com/hapijs/joi
-  res.send('OK');
+  const validation = Joi.validate(req.body, searchSchema);
+  logger.debug({ body: req.body, validation }, '[SERVER.routes.search] Body validation');
+
+  if (validation.error) {
+    res.sendStatus(httpStatus.BAD_REQUEST);
+  } else {
+    res.status(httpStatus.OK).send('OK');
+  }
   next();
 }
 
