f9c48d88a67fed97610ba82106df668abca53698
Louis Ledey <lledey@gmail.com>
2016-02-18 00:46:18 +0100

    Add script to execute manually to update parkings

---
+++ scripts/opendata_manual_update.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const co = require('co');
+const logger = require('../modules').logger;
+const mongooseHelper = require('../modules').mongooseHelper;
+const opendata = require('../worker').opendata;
+const rollbarHelper = require('../modules').rollbarHelper;
+
+mongooseHelper.connect()
+  .then(() => rollbarHelper.init())
+  .then(() => {
+    logger.info('[SCRIPT.opendata_manual_update] Updating Paris...');
+    return co(opendata.paris.update);
+  })
+  .then(() => {
+    logger.info('[SCRIPT.opendata_manual_update] Ended');
+    process.exit(0);
+  })
+  .catch(err => {
+    logger.error(err, '[SCRIPT.opendata_manual_update] Uncaught error');
+    rollbarHelper.rollbar.handleError(err, '[SCRIPT.opendata_manual_update] Uncaught exception');
+    process.exit(1);
+  });
