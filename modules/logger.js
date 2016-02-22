5187c715fa8b7989f7d8b63877797dd429acb46a
Louis Ledey <lledey@gmail.com>
2016-02-23 00:05:43 +0100

    Print prettyfied logs in tests

--- modules/logger.js
+++ modules/logger.js
@@ -3,9 +3,24 @@
 const bunyan = require('bunyan');
 const config = require('config');
 
+const stream = {
+  level: config.log.level,
+  stream: process.stdout
+};
+
+if (process.env.NODE_ENV === 'test') {
+  const PrettyStream = require('bunyan-prettystream');
+  const prettyStdOut = new PrettyStream();
+
+  prettyStdOut.pipe(process.stdout);
+
+  stream.stream = prettyStdOut;
+  stream.type = 'raw';
+}
+
 const logger = bunyan.createLogger({
   name: 'carparker-server',
-  stream: process.stdout,
+  streams: [stream],
   level: config.log.level,
   serializers: {
     err: bunyan.stdSerializers.err,
