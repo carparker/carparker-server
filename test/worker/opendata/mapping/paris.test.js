12329afbde3861d37f2f97fac5284c98b622aa87
Louis Ledey <lledey@gmail.com>
2016-03-01 20:15:28 +0100

    Add tests on Paris opendata mapper

---
+++ test/worker/opendata/mapping/paris.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const moment = require('moment');
+const sinon = require('sinon');
+
+const mapper = require('../../../../worker/opendata/mapping/paris.js');
+
+const input = require('./paris.in.json');
+const output = require('./paris.out.json');
+
+function formatDate(data) {
+  if (data.last_update) {
+    data.last_update = moment(data.last_update).toDate();
+  }
+  return data;
+}
+
+describe.only('[WORKER] mapper.paris', () => {
+  let clock;
+
+  before(done => {
+    clock = sinon.useFakeTimers(1420070400 * 1000);
+    done();
+  });
+
+  after(done => {
+    clock.restore();
+    done();
+  });
+
+  for (let i = 0; i < input.parkings.length; ++i) {
+    it('should correctly map the data', () => {
+      expect(mapper(input.parkings[i])).to.deep.equal(formatDate(output.parkings[i]));
+    });
+  }
+});
