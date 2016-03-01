4657700e78bced4f1a0f1924dc149984a0a5ff5e
Louis Ledey <lledey@gmail.com>
2016-03-01 15:22:24 +0100

    Start server only if launched as stanalone (i.e. not in tests)

--- server.js
+++ server.js
@@ -11,15 +11,31 @@ const server = express();
 
 require('./routes').config(server);
 
-rollbarHelper.init()
-  .then(() => mongooseHelper.connect())
-  .then(() => {
-    const listener = server.listen(server.get('port'), () => {
-      logger.info({ port: listener.address().port, environment: process.env.NODE_ENV }, '[SERVER] Server started');
-    });
-  })
-  .catch(err => {
+function start() {
+  return rollbarHelper.init()
+    .then(() => mongooseHelper.connect())
+    .then(() => {
+      const listener = server.listen(server.get('port'), () => {
+        logger.info({ port: listener.address().port, environment: process.env.NODE_ENV }, '[SERVER] Server started');
+      });
+    })
+    .catch(err => shutdown(err));
+}
+
+function* shutdown(err) {
+  if (err) {
     logger.error(err, '[SERVER] Uncaught error');
     rollbarHelper.rollbar.handleError(err, '[SERVER] Uncaught exception');
-    process.exit(1);
-  });
+  }
+  process.exit(err ? 1 : 0);
+}
+
+if (!module.parent) {
+  start();
+} else {
+  module.exports = {
+    server,
+    start,
+    shutdown
+  };
+}
