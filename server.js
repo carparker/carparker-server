32bf1f5f274d8c2b95d8ea676c1e76e63379c2e0
Louis Ledey <lledey@gmail.com>
2016-03-05 14:48:17 +0100

    Use http instead of express wrapper to listen

--- server.js
+++ server.js
@@ -2,32 +2,46 @@
 
 if (process.env.NEW_RELIC_LICENSE_KEY) require('newrelic');
 
+const config = require('config');
 const express = require('express');
+const http = require('http');
 const logger = require('./modules').logger;
 const mongooseHelper = require('./modules').mongooseHelper;
 const rollbarHelper = require('./modules').rollbarHelper;
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
 
-require('./routes').config(server);
+require('./routes').config(app);
 
 function start() {
   return rollbarHelper.init()
     .then(() => mongooseHelper.connect())
     .then(() => {
-      const listener = server.listen(server.get('port'), () => {
-        logger.info({ port: listener.address().port, environment: process.env.NODE_ENV }, '[SERVER] Server started');
+      server.listen(config.port, () => {
+        logger.info({ port: config.port, environment: process.env.NODE_ENV }, '[SERVER] Server started');
       });
     })
     .catch(err => shutdown(err));
 }
 
-function shutdown(err) {
+function shutdown(err, callback) {
   if (err) {
     logger.error(err, '[SERVER] Uncaught error');
     rollbarHelper.rollbar.handleError(err, '[SERVER] Uncaught exception');
   }
-  process.exit(err ? 1 : 0);
+
+  server.close(closeErr => {
+    if (closeErr) {
+      logger.error(closeErr, '[SERVER] Error closing the server');
+      rollbarHelper.rollbar.handleError(closeErr, '[SERVER] Error closing the server');
+    } else {
+      logger.info('[SERVER] Server closed');
+    }
+    process.exit(err ? 1 : 0);
+    // XXX: callback used for tests, process.exit should be stubed in the case
+    if (callback) callback(closeErr);
+  });
 }
 
 if (!module.parent) {
